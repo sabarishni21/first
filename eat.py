5eab3958ae9994dac8cae0d88ce61720de1f0207
sabarishni21 <sabarishni.krishnakumar@gmail.com>
2022-07-08 22:53:17 +0530

    ghee

--- eat.py
+++ eat.py
@@ -2,5 +2,6 @@ dish = input('What is your afternoon favorite dish?')
 if dish=='white rice':
     print('Good choice!')
     print('Have it daily!')
+    print('Add ghee!')
 else:
     print('Interesting choice!')
